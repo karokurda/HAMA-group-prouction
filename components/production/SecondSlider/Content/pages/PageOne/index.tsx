35f9d13bb92dcfa07425563b98be3ad604922bde
karokurda <96546482+karokurda@users.noreply.github.com>
2022-08-28 13:41:23 +0300

    ferst comek

--- components/production/SecondSlider/Content/pages/PageOne/index.tsx
+++ components/production/SecondSlider/Content/pages/PageOne/index.tsx
@@ -17,7 +17,7 @@ const PageOne: FC = () => {
         backgroundPosition: 'center',
         width:'full',
       }}
-      className="h-[400px] sm:min-h-screen text-white gap-0"
+      className="h-[600px] sm:min-h-screen text-white bottom-0"
     >
       {/* <Container className="pt-40">
         <div className="text-4xl lg:text-6xl font-helvetica-black font-bold uppercase">
