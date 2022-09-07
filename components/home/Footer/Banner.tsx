ac23928d1655255f53666c2d75d0600a4f2f2f49
karokurda <96546482+karokurda@users.noreply.github.com>
2022-09-07 13:42:16 +0300

    ferst comek

--- components/home/Footer/Banner.tsx
+++ components/home/Footer/Banner.tsx
@@ -14,10 +14,10 @@ const Banner = () => {
     <div
       ref={ref}
       onClick={redirectHandler}
-      className="border-8 font-helvetica-condensed uppercase border-white cursor-pointer px-10 py-20 mt-16 text-6xl select-none font-extrabold text-white"
+      className="border-8 font-helvetica-condensed uppercase border-white cursor-pointer px-10 py-20 mt-16 text-4xl select-none font-extrabold text-white"
     >
       <p>Contact us</p>
-      <p></p>
+      
       <div
         className={`mt-2 bg-white ml-2 h-[5px] duration-200 ${
           hovered ? 'w-20' : 'w-10'
